e9061903257d176fdab0464171f359b192f90b73
AbdullahAldakhiel <38670228+AbdullahAldakhiel@users.noreply.github.com>
2018-11-06 18:27:43 +0300

    PA3 Rep Created

---
+++ src/LinkedBT.java
@@ -0,0 +1,58 @@
+
+public class LinkedBT<T> implements BT<T> {
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean full() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean find(Relative rel) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T retrieve() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(T val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean insert(T val, Relative rel) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void deleteSub() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isLeaf() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public BTNode<T> getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
