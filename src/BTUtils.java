46934993e54b6d1b6f525b56d56fb54a6c9561e5
AbdullahAldakhiel <38670228+AbdullahAldakhiel@users.noreply.github.com>
2018-11-10 15:11:20 +0300

    2 diiid

--- src/BTUtils.java
+++ src/BTUtils.java
@@ -4,7 +4,7 @@ public class BTUtils {
 		
 		
 		// First Method ..
-
+/*
 		public static <T> int nbLeaf(BT<T> bt, T e) {
 			bt.find(Relative.Root);
 			if(bt.empty()) {
@@ -64,7 +64,7 @@ public class BTUtils {
 			}
 		}
 		
-		
+		*/
 
 
 
@@ -72,137 +72,66 @@ public class BTUtils {
 	
 	
 		public static <T> void pruneBranch(BT<T> bt, T e) {	
-		bt.find(Relative.Root);
-		recursivePruneBranch(bt, e);
-		if(bt.find(Relative.Root)) {
-			recursivePruneBranch2(bt, e);
-		}
+			bt.find(Relative.Root);
+			if(bt.empty()) {
+				return;
+			}
+			boolean flag = false;
+			boolean leftChecked =false;
+			boolean rightChecked = false;
+					recursivePruneBranch(bt, e, flag, leftChecked,rightChecked);
 	}
 		
-		private static <T> int recursivePruneBranch(BT<T> bt, T e) {
-		
-		if(bt.empty()) {
-			return 0;
-		}
-		if(bt.isLeaf()) {
-						if(bt.retrieve().equals(e)) {
-							bt.deleteSub();
-						
-							return 1;
-						}else {
-							
-							return 0;
-						}
-		}else {
-			if(bt.retrieve().equals(e)) {
+		private static <T> int recursivePruneBranch(BT<T> bt, T e , boolean flag, boolean leftChecked,  boolean rightChecked) {
+			if(bt.retrieve().equals(e) ) {
+				System.out.println(bt.retrieve());
 				bt.deleteSub();
+				if(flag) {
+					bt.find(Relative.Parent);
+					//bt.find(Relative.RightChild);
+				}
 				return 1;
 			}
+			if( bt.isLeaf() ) {
+	
+					
+					bt.find(Relative.Parent);
+					if(flag) {
+						bt.find(Relative.Parent);
+						//bt.find(Relative.RightChild);
+					}
+					
+					return 1;
+			
+			}
+			
+
+			return checkTrueFalse(bt, e, 1, flag, leftChecked, rightChecked) + checkTrueFalse(bt, e, 2, flag, leftChecked, rightChecked);
 			
 			
-				return checkTrueFalse(bt, e, 1) + checkTrueFalse(bt, e, 2) ;
-				//+ checkTrueFalse(bt.find(Relative.RightChild), bt, e, 2);
 			
-		}
 	}
-		private static <T> int checkTrueFalse(BT<T> bt, T e, int i) {
-		if(i==1) {
-			bt.find(Relative.LeftChild);
-
-			return recursivePruneBranch(bt, e);
-		}else {
-			bt.find(Relative.RightChild);
-
-			return recursivePruneBranch(bt, e);
-		}
+		private static <T> int checkTrueFalse(BT<T> bt, T e ,int i, boolean flag, boolean leftChecked, boolean rightChecked) {
+			
+			leftChecked = false;
+			rightChecked = false;
+			flag = false;
+			if(i==1) {
+				leftChecked = bt.find(Relative.LeftChild);
+				
+				if(!leftChecked)return 0;
+				
+				return recursivePruneBranch(bt, e, flag, leftChecked, rightChecked);
+			}else {
+				//bt.find(Relative.Parent);
+				rightChecked = bt.find(Relative.RightChild);
+				flag = true;
+				if (!rightChecked)return 0;
+				
+				return recursivePruneBranch(bt, e , flag, leftChecked, rightChecked);
+			}
 	}
 		
-		private static <T> int recursivePruneBranch2(BT<T> bt, T e) {
-			if(bt.empty()) {
-				return 0;
-			}
-			if(bt.isLeaf()) {
-							if(bt.retrieve().equals(e)) {
-								System.out.println("leaf deleted");
-								bt.deleteSub();
-							
-								return 1;
-							}else {
-								
-								return 0;
-							}
-			}else {
-				if(bt.retrieve().equals(e)) {
-					bt.deleteSub();
-					System.out.println("D");
-					return 1;
-				}
-				
-				
-					return checkTrueFalse2(bt, e, 1) + checkTrueFalse2(bt, e, 2) ;
-					//+ checkTrueFalse(bt.find(Relative.RightChild), bt, e, 2);
-				
-			}
-		}
-		private static <T> int checkTrueFalse2(BT<T> bt, T e, int i) {
-			if(i==1) {
-				bt.find(Relative.RightChild);
-
-				return recursivePruneBranch2(bt, e);
-			}else {
-				bt.find(Relative.LeftChild);
-
-				return recursivePruneBranch2(bt, e);
-			}
-		}
-
-
-//		if(find) {
-//			
-//			if(i==1) {
-//				return RecurNbLeaf(bt, e);
-//			}else {
-//				bt.find(Relative.Parent);
-//				//bt.find(Relative.Parent);
-//				//bt.find(Relative.LeftChild);
-//				return RecurNbLeaf(bt, e);
-//			}
-//			
-//		}else if(!find) {
-//			if(i==2) {
-//				bt.find(Relative.Parent);
-//				bt.find(Relative.LeftChild);
-//				return RecurNbLeaf(bt, e);
-//			}
-//		}
-//		return 0;
-		/*
-		if(find) {
-			
-			if(i==1) {
-				 return recursivePruneBranch(bt, e);
-			}else {
-				
-			//	bt.find(Relative.Parent);
-			//bt.find(Relative.Parent);
-
-			//bt.find(Relative.LeftChild);
-				return recursivePruneBranch(bt, e);
-				
-
-			}
-			
-		}else if(!find) {
-			if(i==2) {
-				bt.find(Relative.Parent);
-				bt.find(Relative.LeftChild);
-				return recursivePruneBranch(bt, e);
-			}else {
-				return 0;	
-				}
-		}
-		return 0;
-	*/
 	
 	
 		
@@ -227,8 +156,11 @@ public class BTUtils {
 				c.insert(180, Relative.RightChild);
 				c.print(c.getRoot());
 				System.out.println("===");
-				System.out.println(nbLeaf(c, 180));
-
+				//System.out.println(nbLeaf(c, 180));
+				
+				pruneBranch(c,30);
+				System.out.println("===");
+				c.print(c.getRoot());
 			}
 
 
