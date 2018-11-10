06ea391e24c85425419aeeadeffeab96f7958479
AbdullahAldakhiel <38670228+AbdullahAldakhiel@users.noreply.github.com>
2018-11-10 14:51:20 +0300

    didddd

--- src/BTUtils.java
+++ src/BTUtils.java
@@ -2,81 +2,69 @@
 public class BTUtils {
 
 		
-		private static int count=0;
+		
 		// First Method ..
 
 		public static <T> int nbLeaf(BT<T> bt, T e) {
 			bt.find(Relative.Root);
-			T Tmp = bt.retrieve();
-
-			int tm = RecNbLeaf(bt, e, Tmp);
-			tm += RecNbLeaf2(bt, e, Tmp);
-			return tm ;
-			
-		}
-		private static <T> int RecNbLeaf(BT<T> bt, T e , T Tmp) {
 			if(bt.empty()) {
 				return 0;
 			}
+			boolean flag = false;
+			boolean leftChecked =false;
+			boolean rightChecked = false;
+			return RecNbLeaf(bt, e, flag, leftChecked,rightChecked);
+			//return count ;
+		}
+		private static <T> int RecNbLeaf(BT<T> bt, T e , boolean flag, boolean leftChecked,  boolean rightChecked) {
 			if( bt.isLeaf() ) {
-				if(bt.retrieve().equals(e)) {
+				if(bt.retrieve().equals(e) ) {
+					System.out.println(bt.retrieve());
+					bt.find(Relative.Parent);
+					if(flag) {
+						bt.find(Relative.Parent);
+						//bt.find(Relative.RightChild);
+					}
+					
 					return 1;
 				}else {
+					bt.find(Relative.Parent);
+					
+					if(flag) {
+						bt.find(Relative.Parent);
+						//bt.find(Relative.RightChild);
+					}
 					return 0;
 				}
 			}
 			
-			count++;
-			return RecNbLeaf2(bt, e, 1, Tmp) + RecNbLeaf2(bt, e, 2, Tmp);
+			
+			return RecNbLeaf2(bt, e, 1, flag, leftChecked, rightChecked) + RecNbLeaf2(bt, e, 2, flag, leftChecked, rightChecked);
 			
 			
 			
 		}
-		private static <T> int RecNbLeaf2(BT<T> bt, T e ,int i,T Tmp) {
+		private static <T> int RecNbLeaf2(BT<T> bt, T e ,int i, boolean flag, boolean leftChecked, boolean rightChecked) {
+			leftChecked = false;
+			rightChecked = false;
+			flag = false;
 			if(i==1) {
-				bt.find(Relative.LeftChild);
-				return RecNbLeaf(bt, e, Tmp);
+				leftChecked = bt.find(Relative.LeftChild);
+				
+				if(!leftChecked)return 0;
+				
+				return RecNbLeaf(bt, e, flag, leftChecked, rightChecked);
 			}else {
-				bt.find(Relative.Parent);
-				if(bt.retrieve().equals(Tmp)) {
-					return 0;
-				}
-				bt.find(Relative.RightChild);
-				return RecNbLeaf(bt, e, Tmp);
+				//bt.find(Relative.Parent);
+				rightChecked = bt.find(Relative.RightChild);
+				flag = true;
+				if (!rightChecked)return 0;
+				
+				return RecNbLeaf(bt, e , flag, leftChecked, rightChecked);
 			}
 		}
 		
-		private static <T> int RecNbLeaf2(BT<T> bt, T e , T Tmp) {
-			if(bt.empty()) {
-				return 0;
-			}
-			if( bt.isLeaf() ) {
-				if(bt.retrieve().equals(e)) {
-					return 1;
-				}else {
-					return 0;
-				}
-			}
-			
-			count++;
-			return RecNbLeaf22(bt, e, 1, Tmp) + RecNbLeaf22(bt, e, 2, Tmp);
-			
-			
-			
-		}
-		private static <T> int RecNbLeaf22(BT<T> bt, T e ,int i,T Tmp) {
-			if(i==1) {
-				bt.find(Relative.RightChild);
-				return RecNbLeaf2(bt, e, Tmp);
-			}else {
-				bt.find(Relative.Parent);
-				if(bt.retrieve().equals(Tmp)) {
-					return 0;
-				}
-				bt.find(Relative.LeftChild);
-				return RecNbLeaf2(bt, e, Tmp);
-			}
-		}
+		
 
 
 
@@ -224,80 +212,22 @@ public class BTUtils {
 				
 
 				c.insert(10, Relative.Root);
-				c.insert(15, Relative.LeftChild);
+			
+				c.insert(20, Relative.LeftChild);
+				c.insert(180, Relative.RightChild);
+				c.find(Relative.Parent);
 				c.insert(30, Relative.LeftChild);
-				c.find(Relative.Parent);
-				c.insert(50, Relative.RightChild);
 				c.find(Relative.Root);
-				c.insert(17, Relative.RightChild);
-				c.insert(14, Relative.LeftChild);
+				c.insert(50, Relative.RightChild);
+				c.insert(30, Relative.RightChild);
 				c.find(Relative.Parent);
-				c.insert(16, Relative.RightChild);
-				c.insert(17, Relative.LeftChild);
-				c.find(Relative.Parent);
-				c.insert(16, Relative.RightChild);
-
-				int leaf = nbLeaf(c ,50);
-				System.out.println(leaf);
-				
-
-//				LinkedBT<Integer> c = new LinkedBT<Integer>();
-				
-
-//				c.insert(10, Relative.Root);
-//				c.insert(15, Relative.LeftChild);
-//				c.insert(16, Relative.LeftChild);
-//				c.find(Relative.Parent);
-//				c.insert(16, Relative.RightChild);
-//				c.insert(6, Relative.RightChild);
-//				c.insert(5, Relative.RightChild);
-//				c.insert(4, Relative.RightChild);
-//				c.insert(3, Relative.RightChild);
-//				c.insert(2, Relative.RightChild);
-//				c.insert(1, Relative.RightChild);
-//				c.find(Relative.Root);
-//				c.insert(17, Relative.RightChild);
-//				c.insert(10, Relative.LeftChild);
-//				c.find(Relative.Parent);
-//				c.insert(16, Relative.RightChild);
-//				c.insert(17, Relative.LeftChild);
-//				c.find(Relative.Parent);
-//				c.insert(16, Relative.RightChild);
-//				c.insert(6, Relative.RightChild);
-//				c.insert(5, Relative.RightChild);
-//				c.insert(4, Relative.RightChild);
-//				c.insert(3, Relative.RightChild);
-//				c.insert(2, Relative.RightChild);
-//				c.insert(1, Relative.RightChild);
-				
-
-//				c.insert(1, Relative.Root);
-//				c.insert(2, Relative.RightChild);
-//				c.insert(3, Relative.RightChild);
-//				c.insert(4, Relative.RightChild);
-//				c.insert(5, Relative.RightChild);
-//				c.insert(6, Relative.RightChild);
-//				c.insert(7, Relative.RightChild);
-//				c.insert(8, Relative.RightChild);
-//				c.insert(9, Relative.RightChild);
-//				c.find(Relative.Root);
-//				c.insert(1, Relative.Root);
-//				c.insert(2, Relative.LeftChild);
-//				c.insert(3, Relative.LeftChild);
-//				c.insert(4, Relative.LeftChild);
-//				c.insert(5, Relative.LeftChild);
-//				c.insert(6, Relative.LeftChild);
-//				c.insert(7, Relative.LeftChild);
-//				c.insert(8, Relative.LeftChild);
-//				c.insert(9, Relative.LeftChild);
-				
-
-
-				//int leaf = nbLeaf(c ,9);
-
-
-				
-
+				c.insert(30, Relative.LeftChild);
+				c.insert(60, Relative.RightChild);
+				c.insert(120, Relative.LeftChild);
+				c.insert(180, Relative.RightChild);
+				c.print(c.getRoot());
+				System.out.println("===");
+				System.out.println(nbLeaf(c, 180));
 
 			}
 
